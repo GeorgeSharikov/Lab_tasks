84117488ddeae40a48c72ac02c017c539434fad0
John Doe <johndoe@example.com>
2024-04-05 06:24:41 +0300

    Final version

--- lab_8/main.c
+++ lab_8/main.c
@@ -5,26 +5,6 @@
 
 #define MAX_SZ 256
 
-typedef struct node {
-    char* name;
-    char* street;
-    int building;
-    struct node *next;
-} node;
-
-
-void list_push(node** head, char* street, int building, char* name) {
-    //printf("PUSH\n");
-    node* tmp = (node*)malloc(sizeof(node));
-    //printf("%s", street);
-    tmp->street = street;
-    //printf("%s", tmp->street);
-    tmp->building = building;
-    tmp->name = name;
-    tmp->next = (*head);
-    (*head) = tmp;   
-}
-
 uint32_t get_hash(unsigned char* key, int len)
 {
     uint32_t hash, i;
@@ -41,26 +21,50 @@ uint32_t get_hash(unsigned char* key, int len)
 }
 
 int get_index(char* item) {
-    int hash = get_hash(item, sizeof(item));
+    int hash = get_hash(item, strlen(item));
+    
     int index = hash % 23;
-    return index > 0 ? index : index*(-1);
+    return index > 0 ? index : index * (-1);
+}
+
+typedef struct node {
+    char* name;
+    char* street;
+    int building;
+    struct node *next;
+} node;
+
+
+void push(node** head, char* street, int building, char* name) {
+    node* tmp = (node*)malloc(sizeof(node));
+    tmp->street = street;
+    tmp->building = building;
+    tmp->name = name;
+    tmp->next = (*head);
+    (*head) = tmp;
 }
 
 void search(node *item, char* street_par, int building_par) {
-    while (item->next) {
-        //printf("%s\n", item->street);
-        //printf("%s\n", item->name);
-        //printf("%d\n", item->building);
-        //printf("LOLOLO");
-        printf("%sc\n", street_par);
-        printf("%sc\n", item->street);
-        printf("%d\n", street_par == item->street);
-
-        if (item->street == street_par) {
-            printf("Final %s", item->name);
+    do {
+        if (strcmp(item->street, street_par) == 0) {
+            if (building_par == 0) {
+                printf("%s\n", item->name);
+            }
+            else {
+                if (building_par == item->building) {
+                    printf("%s\n", item->name);
+                }
+            }
         }
+        //printf("%s", street_par);
+        //printf("%s\n", item->street);
+        //printf("%d\n", strcmp(item->street, street_par));
+        //printf("FINALLY\n");
+        //printf("Street is %s\n", item->street);
+        //printf("Build is %d\n", item->building);
+        //printf("Name is %s\n", item->name);
         item = item->next;
-    }
+    } while (item != NULL);
 }
 
 int main()
@@ -69,7 +73,9 @@ int main()
      printf("Street: ");
      fgets(street_par, MAX_SZ, stdin);
      if ((strlen(street_par) > 0) && (street_par[strlen(street_par) - 1] == '\n'))
-         street_par[strlen (street_par) - 1] = '';
+         street_par[strlen (street_par) - 1] = '\0';
+     char* street_par_n = malloc(strlen(street_par));
+     sprintf(street_par_n, " %s", street_par);
      printf("\n");
 
      printf("Building: ");
@@ -80,60 +86,65 @@ int main()
      if (strlen(building) == 0) building = "0";
      int building_par = atoi(building);
      printf("\n");
-    // printf("%s\n", street);
-    // printf("%d", building=="");
-    
-
+   
     char buffer[256];
     FILE* fp = fopen("mock.txt", "r");
     int str_count = 0;
 
     node* arr[23];
-    for (int i = 0; i < sizeof(arr)/sizeof(node); i++) arr[i] = NULL;
+    for (int i = 0; i < 23; i++)
+    {
+        arr[i] = NULL;
+    }
 
     while (fgets(buffer, sizeof(buffer), fp)) {
         str_count += 1;
 
-        char * token = strtok(buffer, ";");
+        char* token = strtok(buffer, ";");
 
         char* name[3];
         int name_len = 0;
         char* address;
         int c = 0;
-        while( token != NULL ) {
-            if(c <= 2){
-                //printf("%s\n", token);
+        while (token != NULL) {
+            if (c <= 2) {
                 name[c] = token;
-                name_len+=strlen(token);              
-            }else if(c == 3) {
-                address = token;
+                name_len += strlen(token);
             }
-            c+=1;
+            else if (c == 3) {
+                address = token;
+                
+                break;
+            }
+            c += 1;
             token = strtok(NULL, ";");
         }
-        char* street = strtok(address, ",");
-        int building = atoi(strtok(NULL, ","));
-        //printf("%s, %d\n", street, building);
         
-        //printf("%s\n", name[2]);  
-        char *full_name = (char *)malloc(name_len+1);
-        snprintf( full_name, name_len+1, "%s%s%s", name[0], name[1], name[2]);
-        /*printf("%s;%s\n",full_name, street);*/
+        char* old_street = strtok(address, ",");
+        char* street = malloc(strlen(old_street) + 1);
+        snprintf(street, strlen(old_street) + 1, "%s", old_street);
+        int building = atoi(strtok(NULL, ","));
+
+        char* full_name = malloc(name_len + 1);
+        snprintf(full_name, name_len + 1, "%s%s%s", name[0], name[1], name[2]);
 
         int item_index = get_index(street);
-        /*printf*/("%s: %d\n", street, item_index);
-        if (arr[item_index] == NULL) {
-            node* head = NULL;
-            arr[item_index] = head;
-        }
-        //printf("List push: ", street);
-        list_push(&arr[item_index], street, building, full_name);
+        //printf("%s : %d\n", street, item_index);
+
+        push(&arr[item_index], street, building, full_name);
+
     }
-    //printf(arr[21]->street);
-    int ind = get_index(street_par);
-    printf("INDEX %d\n", ind);
+    //printf("%s : %s\n", arr[3]->name, arr[3]->street);
+    //printf("%s : %s\n", arr[14]->name, arr[14]->street);
+    //printf("%s : %s\n", arr[14]->next->name, arr[14]->next->street);
+    //printf("%s : %s\n", arr[14]->next->next->name, arr[14]->next->next->street);
+    //printf("%s : %s\n", arr[7]->name, arr[7]->street);
+    //printf("%s : %s\n", arr[7]->next->name, arr[7]->next->street);
+    //printf("%s : %s\n\n", arr[11]->name, arr[11]->street);
+
+    int ind = get_index(street_par_n);
     
-    search(arr[ind], street_par, building_par);
+    search(arr[ind], street_par_n, building_par);
     fclose(fp);
 
     return 0;
